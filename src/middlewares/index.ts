aada1b16c005f2c3e2dc685ef9baf6b170c563ad
Cyril Urban <cyril.urban@qest.cz>
2019-11-01 10:44:23 +0100

    fix(serviceUnavailable): fix export of function serviceUnavailable

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -3,4 +3,5 @@ export { logRequest } from './log-request';
 export { notFoundHandler } from './not-found-handler';
 export { rateLimiter } from './rate-limiter';
 export * from './base-front-route';
+export { serviceUnavailable } from './service-available';
 export { corsSetup } from './cors-setup';
