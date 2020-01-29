8f8ccce6abb8ea55aa809ac90acc5d27a46f67b2
Luboš Růžička <lubos@ruzaweb.cz>
2020-01-29 22:23:27 +0100

    fix(exports): fix error of missing interface

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export * from './server';
 export * from './middlewares';
 export * from './base-front-routes';
-export { IExpressAppMiddlewareConfig, IExpressApp, IExpressConfig, IBasicUser, IBasicAppRequest } from './interfaces';
+export { IExpressApp, IExpressConfig, IBasicUser, IBasicAppRequest } from './interfaces';
